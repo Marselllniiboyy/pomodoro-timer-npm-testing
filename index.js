ec9f2d3a1ec01dfae810f2e0d7b1e1dfe1955745
Marselllniiboyy <marseljayawijaya@gmail.com>
2025-09-07 23:47:31 +0800

    Make Pomodoro-Timer With pacage node-notifier and moment, with some recursive function init, thx

---
+++ index.js
@@ -0,0 +1,43 @@
+const notifier = require("node-notifier");
+const moment = require("moment");
+
+const argTime = process.argv.slice(2);
+
+const POMODORO_DURATION = argTime[0];
+const BREAK_DURATION = argTime[1];
+
+let isWoking = false;
+let remainingTime = 0;
+
+function formatingTime(totalSecond) {
+  const duration = moment.duration(totalSecond, "seconds");
+  const hours = duration.hours().toString().padStart(2, "0");
+  const minutes = duration.minutes().toString().padStart(2, "0");
+  const seconds = duration.seconds().toString().padStart(2, "0");
+
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+function startTimer(duration) {
+  isWoking = !isWoking;
+  remainingTime = duration * 60;
+
+  const timer = setInterval(() => {
+    remainingTime--;
+    const formattedTime = formatingTime(remainingTime);
+    console.log(`${isWoking ? "Work" : "Break"}: ${formattedTime}`);
+
+    if (remainingTime === 0) {
+      clearInterval(timer);
+      notifier.notify({
+        title: isWoking ? "Break Time" : "Working Time",
+        message: isWoking ? "Good Work" : "Good Break",
+        sound: true,
+        wait: true,
+      });
+      startTimer(isWoking ? BREAK_DURATION : POMODORO_DURATION);
+    }
+  }, 1000);
+}
+
+startTimer(POMODORO_DURATION);
